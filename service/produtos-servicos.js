9bd24d75518dd1832b47a3e460e53b7e4a111493
BlackStarSt <sthefanya86@gmail.com>
2022-08-11 17:50:56 -0300

    Correção do bug ao adicionar

--- service/produtos-servicos.js
+++ service/produtos-servicos.js
@@ -5,23 +5,23 @@ const listaProdutos = () => fetch('http://localhost:3000/produtos').then(respost
 const criaProduto = (name, imageURL, price, category, description) => {
     return fetch('http://localhost:3000/produtos', {
         method: 'POST',
-        headers: {
-            'Content-type' : 'application/json'
-        },
-        body: JSON.stringify({
-            name,
-            imageURL, 
-            price,
-            category,
-            description
+            headers: {
+                'Content-type' : 'application/json'
+            },
+            body: JSON.stringify({
+                name,
+                imageURL, 
+                price,
+                category,
+                description
+            }) 
         })
         .then(resposta => {
             if(resposta.ok) {
                 return resposta.body;
             }
             throw new Error('Não foi possível criar o produto');
-        })
-    });
+        });
 };
 
 export const produtoServices = {
