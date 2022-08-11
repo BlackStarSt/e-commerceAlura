63947f4dcbb7379007b1e5cd1cb768d2c63277c0
BlackStarSt <sthefanya86@gmail.com>
2022-08-11 17:00:29 -0300

    Mudança no botão do banner

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,8 +2,3 @@ const botaoLogin = document.getElementById('botaoLogin');
 botaoLogin.addEventListener('click', function() {
     window.location.href = './login.html';
 });
-
-const botaoBanner = document.getElementById('botaoBanner');
-botaoBanner.addEventListener('click', function() {
-    window.location.href = './todos_produtos.html';
-});
